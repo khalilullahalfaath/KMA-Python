7936628378d2ef47457839316980afb72ad8b58a
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-08-25 07:54:55 +0700

    complete unimodal benchmark function

--- KMA-Python/evaluation.py
+++ KMA-Python/evaluation.py
@@ -5,17 +5,38 @@ def evaluation(x):
     dim = len(x)
 
     match function_ID:
+    ############# unimodal function ################
         case 1:
-            fx = sum(x ** 2)
+            # sphere function
+            fx = 0
+            for i in range(dim):
+                fx += x[i] ** 2
         case 2:
+            # schwefel 2.22 function
             fx = sum(abs(x)) + np.prod(abs(x))
         case 3:
+            # schwefel 1.2 function
             fx = 0
             for i in range(dim):
                 fx += sum(x[:i]) ** 2
         case 4:
+            # schwefel 2.21 function
             fx = max(abs(x))
         case 5:
-            fx = sum(100 * (x[1:] - x[:-1] ** 2) ** 2 + (x[:-1] - 1) ** 2)
+            # rosenbrock function
+            fx = sum(100 * (x[1:dim] - x[:dim-1] ** 2) ** 2 + (x[:dim-1] - 1) ** 2)
+        case 6:
+            # step function
+            fx = sum(np.floor((x + 0.5)) ** 2)
+        case 7:
+            # quartic function
+            fx = 0
+            for i in range(dim):
+                fx += (i + 1) * x[i] ** 4
+            fx += np.random.rand()
 
+    ############# multimodal function ################
+        case 8:
+            # Schwefel function
+            fx = sum(-x * np.sin(np.sqrt(abs(x))))
     return fx

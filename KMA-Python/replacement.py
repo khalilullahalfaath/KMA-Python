c7141930c8fb267a5dbf779eb8e608dcd4ca5ac2
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-09-06 09:27:44 +0700

    add function for replacement

---
+++ KMA-Python/replacement.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def replacement(x, fx, y, fy):
+    LX = x.shape[0]
+    XY = np.concatenate((x, y), axis=0)
+    FXFY = np.concatenate((fx, fy), axis=0)
+
+    # sort the population
+    # TODO: check if this is the correct way to sort, otherwise replace axis with 0
+    SortedVal=FXFY.sort(axis=1)
+    SortedInd=FXFY.argsort(axis=1)
+    z = XY[SortedInd, ]
+    fz = SortedVal[:LX]
